68372e0ee27f959421239af560147b65b9467c65
göknur <goknur.bulbul1166@gmail.com>
2022-12-27 13:45:16 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi, toplam = 0;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz:" );
+            sayi = input.nextInt();
+
+                if(sayi % 4 == 0){
+                    toplam+=sayi;
+                }
+        }while(sayi%2 == 0);
+        System.out.print("Sayıların toplamı:" +toplam );
+
+    }
+}
